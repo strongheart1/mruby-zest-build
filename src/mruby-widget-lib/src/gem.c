12de0c737350f507414a0d9d951008faebad4d4d
Strongheart <strongheart_1@hotmail.com>
2020-03-29 19:19:20 +0200

    neue Modifizierte Logarithmische Skalierungsfunktion in gem.c für Range Min =0

--- src/mruby-widget-lib/src/gem.c
+++ src/mruby-widget-lib/src/gem.c
@@ -716,7 +716,7 @@ remote_cb_127(const char *msg, remote_cb_data *cb)
 {
     //assume the 0..127 integer case for the input
     //assume the 0..1   float   case for the output
-
+	printf("remote_cb_127\n");
     mrb_assert(!strcmp("i",rtosc_argument_string(msg)));
     int arg = rtosc_argument(msg, 0).i;
 
@@ -803,11 +803,18 @@ remote_cb(const char *msg, void *data)
     else if(!strcmp("f", arg_str)) {
         float val = rtosc_argument(msg, 0).f;
 		if(cb->log) {
-            const float b = log(cb->min);
-            const float a = log(cb->max)-b;
-			val = (logf(val)-b)/a; // inverse function of mrb_remote_param_set_value
-			printf("remote_cb:: min:%f, max:%f, a:%f, b:%f, out:%f\n",cb->min, cb->max, a, b, val); // Test  log scaling
-        } else
+			if (cb->min > 0){
+				const float b = log(cb->min);
+				const float a = log(cb->max)-b;
+				val = (logf(val)-b)/a; // inverse function of mrb_remote_param_set_value
+				printf("remote_cb:: val:%f, min:%f, max:%f, a:%f, b:%f, out:%f\n",rtosc_argument(msg, 0).f, cb->min, cb->max, a, b, val); // Test  log scaling
+				}else { // min <= 0
+				const float a = log(4096);
+				const float b = log(cb->max)-a;
+				val = log1pf(val*4096/cb->max)/a; // inverse function of mrb_remote_param_set_value
+				printf("MOD:: remote_cb:: val:%f, min:%f, max:%f, a:%f, b:%f, out:%f\n",rtosc_argument(msg, 0).f, cb->min, cb->max, a, b, val); // Test  log scaling
+				}
+			} else
             val = (val-cb->min)/(cb->max-cb->min);
         mrb_funcall(cb->mrb, cb->cb, "call", 1, mrb_float_value(cb->mrb, val));
     } else if(!strcmp("T", arg_str))
@@ -973,11 +980,18 @@ mrb_remote_param_set_value(mrb_state *mrb, mrb_value self)
         const float x = value;
         float out = 0;
         if(param->scale && strstr(param->scale, "log")) {
-            const float b = log(param->min);
-            const float a = log(param->max)-b;
-            out = expf(a*x+b);
-			printf("Min::%f, max:%f, a:%f, b:%f, out:%f\n",param->min, param->max, a, b, out); // Test  log scaling
-        } else
+			if (param->min > 0) {
+				const float b = log(param->min);
+				const float a = log(param->max)-b;
+				out = expf(a*x+b);
+				printf("in:%f, min:%f, max:%f, a:%f, b:%f, out:%f\n",x , param->min, param->max, a, b, out); // Test  log scaling
+				} else { // min <= 0 ( e.g. envelope time param )
+				const float a = log(4096.0);
+				const float b = log(param->max)-a;
+				out = expm1f(a*x)*param->max/4096.0;
+				printf("MOD:: in:%f, min:%f, max:%f, a:%f, b:%f, out:%f\n",x , param->min, param->max, a, b, out);
+				}
+			} else
             out = (param->max-param->min)*value + param->min;
         br_set_value_float(param->br, param->uri, out);
     }
